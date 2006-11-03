91d73613ccaf480a89c5cf499e65bbc0b09284dd
arthur.taylor <arthur.taylor@noaa.gov>
2006-11-03 15:43:54 +0000

    updating metaname.c to handle the ) in the centers list.  Also updated it to have center 'ARINC'
    git-svn-id: https://vlab.ncep.noaa.gov/svn/degrib/degrib@324 98767674-7b1f-0410-897a-ff3155737c25

--- src/degrib/userparse.h
+++ src/degrib/userparse.h
@@ -1,5 +1,5 @@
 #define PROGRAM_VERSION "1.84"
-#define PROGRAM_DATE "11/01/2006"
+#define PROGRAM_DATE "11/03/2006"
 /*****************************************************************************
  * userparse.h
  *
