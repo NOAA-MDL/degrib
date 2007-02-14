0af1e4afb37a304def3f00ece6f3509f862df0f9
arthur.taylor <arthur.taylor@noaa.gov>
2007-02-14 00:53:07 +0000

    updating setupMin.sh
    git-svn-id: https://vlab.ncep.noaa.gov/svn/degrib/degrib@368 98767674-7b1f-0410-897a-ff3155737c25

--- src/setupMin.sh
+++ src/setupMin.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+echo 'first try to do make -f makefile.win'
+echo "if that doesn't work then try this"
 mkdir ./jpeg2000/src/libjasper/base/.deps
 cp ./jpeg2000/src/libjasper/base/_deps/mingw/* ./jpeg2000/src/libjasper/base/.deps
 mkdir ./jpeg2000/src/libjasper/jpc/.deps
 cp ./jpeg2000/src/libjasper/jpc/_deps/mingw/* ./jpeg2000/src/libjasper/jpc/.deps
+cp ./jpeg2000/libtool.mingw ./jpeg2000/libtool
 mkdir ./libxml/.deps
 cp ./libxml/_deps/mingw/* ./libxml/.deps
