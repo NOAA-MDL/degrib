3a458c17334f130e6d132c1b53d326bc7af6a14a
arthur.taylor <arthur.taylor@noaa.gov>
2007-02-16 23:05:38 +0000

    adding libxml.tar.gz to help in case timestamp problem occurs again with libxml
    git-svn-id: https://vlab.ncep.noaa.gov/svn/degrib/degrib@383 98767674-7b1f-0410-897a-ff3155737c25

--- src/setupMin.sh
+++ src/setupMin.sh
@@ -6,8 +6,10 @@ cp ./jpeg2000/src/libjasper/base/_deps/mingw/* ./jpeg2000/src/libjasper/base/.de
 mkdir ./jpeg2000/src/libjasper/jpc/.deps
 cp ./jpeg2000/src/libjasper/jpc/_deps/mingw/* ./jpeg2000/src/libjasper/jpc/.deps
 cp ./jpeg2000/libtool.mingw ./jpeg2000/libtool
+
 #mkdir ./libxml/.deps
 #cp ./libxml/_deps/mingw/* ./libxml/.deps
 
 echo 'libxml appears to have a time dependence on its files'
-echo 'so you may need the distribution copy in order to get the right times'
+echo 'to resolve this, use tar -xzf libxml.tar.gz to overwrite'
+echo 'the timestamps'
