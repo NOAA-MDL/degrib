c804ae08d7be6c7756c6cae8dd1ae6420d3113f4
arthur.taylor <arthur.taylor@noaa.gov>
2007-02-17 00:50:01 +0000

    needed to reintroduce libxml/_deps to compile a second time without having done a makefile.win clean
    git-svn-id: https://vlab.ncep.noaa.gov/svn/degrib/degrib@389 98767674-7b1f-0410-897a-ff3155737c25

--- src/setupMin.sh
+++ src/setupMin.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
-echo 'first try to do make -f makefile.win'
-echo "if that doesn't work then try this"
-mkdir ./jpeg2000/src/libjasper/base/.deps
-cp ./jpeg2000/src/libjasper/base/_deps/mingw/* ./jpeg2000/src/libjasper/base/.deps
-mkdir ./jpeg2000/src/libjasper/jpc/.deps
-cp ./jpeg2000/src/libjasper/jpc/_deps/mingw/* ./jpeg2000/src/libjasper/jpc/.deps
-cp ./jpeg2000/libtool.mingw ./jpeg2000/libtool
 
-#mkdir ./libxml/.deps
-#cp ./libxml/_deps/mingw/* ./libxml/.deps
+echo "When first setting up in mingw, you may need to"
+echo "use tar -xzf libxml.tar.gz to get the correct timestamps"
+echo "on the libxml files"
 
-echo 'libxml appears to have a time dependence on its files'
-echo 'to resolve this, use tar -xzf libxml.tar.gz to overwrite'
-echo 'the timestamps'
+echo ""
+echo "After that, make -f makefile.win should work the first time"
+echo ""
+echo "to get make -f makefile.win to work without having to do:"
+echo "  make -f makefile.win clean"
+echo "the second time, do the following:"
+echo ""
+echo "cp ./jpeg2000/src/libjasper/base/_deps/mingw/* ./jpeg2000/src/libjasper/base/.deps"
+echo "cp ./jpeg2000/src/libjasper/jpc/_deps/mingw/* ./jpeg2000/src/libjasper/jpc/.deps"
+echo "cp ./jpeg2000/libtool.mingw ./jpeg2000/libtool"
+echo ""
+echo "cp ./libxml/_deps/mingw/* ./libxml/.deps"
+echo ""
+
+# cp ./jpeg2000/src/libjasper/base/_deps/mingw/* ./jpeg2000/src/libjasper/base/.deps
+# cp ./jpeg2000/src/libjasper/jpc/_deps/mingw/* ./jpeg2000/src/libjasper/jpc/.deps
+# cp ./jpeg2000/libtool.mingw ./jpeg2000/libtool
+# cp ./libxml/_deps/mingw/* ./libxml/.deps
+
