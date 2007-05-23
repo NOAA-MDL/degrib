396504d2e859281e66b568b1729210dc2354f03a
arthur.taylor <arthur.taylor@noaa.gov>
2007-05-23 18:53:26 +0000

    Changing pack routines to not ignore idrstmpl[4] = original data field type
    git-svn-id: https://vlab.ncep.noaa.gov/svn/degrib/degrib@464 98767674-7b1f-0410-897a-ff3155737c25

--- src/grib2lib/g2clib-1.0.4/misspack.c
+++ src/grib2lib/g2clib-1.0.4/misspack.c
@@ -515,7 +515,12 @@ void misspack(g2float *fld,g2int ndpts,g2int idrsnum,g2int *idrstmpl,
 //
       mkieee(&rmin,idrstmpl+0,1);   // ensure reference value is IEEE format
       idrstmpl[3]=nbitsgref;
-      idrstmpl[4]=0;         // original data were reals
+//
+//  The user may have intentionally set the field type to 1 (integer) since
+//  their data was integer before casting it to float to call these routines.
+//
+      if (idrstmpl[4] != 1)
+         idrstmpl[4]=0;      // original data were reals
       idrstmpl[5]=1;         // general group splitting
       idrstmpl[9]=ngroups;          // Number of groups
       idrstmpl[10]=ngwidthref;       // reference for group widths
