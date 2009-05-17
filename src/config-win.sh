01b83ca733577c41413747ea72453b5085060e37
arthur.taylor <arthur.taylor@noaa.gov>
2009-05-17 21:44:16 +0000

    Checking in Tcl8.32 configuration as the default.  It now works in 8.5, but the fonts in tkdegrib need to be reviewed before I can upgrade
    git-svn-id: https://vlab.ncep.noaa.gov/svn/degrib/degrib@842 98767674-7b1f-0410-897a-ff3155737c25

--- src/config-win.sh
+++ src/config-win.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-./configure CFLAGS="-O3" TCL_PREFIX=../tcl853_include_win TCL_VERSION=85
+#./configure CFLAGS="-O3" TCL_PREFIX=../tcl853_include_win TCL_VERSION=85
 # ./configure CFLAGS="-O3" TCL_PREFIX=../tcl853_include_win TCL_VERSION=85 --with-halo
+
+./configure CFLAGS="-O3" TCL_PREFIX=c:/tcl832 TCL_VERSION=83
+# ./configure CFLAGS="-O3" TCL_PREFIX=c:/tcl832 TCL_VERSION=83 --with-halo
