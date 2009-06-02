02d4863b7b28ccbbf1d073d01b6193637441f5f5
arthur.taylor <arthur.taylor@noaa.gov>
2009-06-02 01:43:56 +0000

    Adding a line to config-linux.sh which should allow for making the shared library for AWIPS
    git-svn-id: https://vlab.ncep.noaa.gov/svn/degrib/degrib@878 98767674-7b1f-0410-897a-ff3155737c25

--- src/config-linux.sh
+++ src/config-linux.sh
@@ -2,3 +2,4 @@
 ./configure CFLAGS="-O3"
 #configure CFLAGS="-O3" TCL_PREFIX=/usr/local/tcltk
 #configure CFLAGS="-O3" TCL_PREFIX=/usr/local/tcltk --with-halo
+#configure CFLAGS="-O3" --with-halo CC=gcc34
