c63aa8a84ed6a8959449a87cb7eda3f925a19265
michael.allard <michael.allard@noaa.gov>
2016-10-31 14:55:27 +0000

    Rolled back to r1148
    git-svn-id: https://vlab.ncep.noaa.gov/svn/degrib/degrib@1171 98767674-7b1f-0410-897a-ff3155737c25

--- src/degrib/userparse.h
+++ src/degrib/userparse.h
@@ -1,5 +1,5 @@
-#define PROGRAM_VERSION "2.08"
-#define PROGRAM_DATE "11/01/2015"
+#define PROGRAM_VERSION "2.07"
+#define PROGRAM_DATE "08/24/2016"
 /*****************************************************************************
  * userparse.h
  *
