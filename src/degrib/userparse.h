35708bfced3179800f455b55ccf11ba457217815
michael.allard <michael.allard@noaa.gov>
2018-05-21 13:49:49 +0000

    Updating the version number.
    git-svn-id: https://vlab.ncep.noaa.gov/svn/degrib/degrib@1283 98767674-7b1f-0410-897a-ff3155737c25

--- src/degrib/userparse.h
+++ src/degrib/userparse.h
@@ -1,5 +1,5 @@
-#define PROGRAM_VERSION "2.16"
-#define PROGRAM_DATE "5/8/2018"
+#define PROGRAM_VERSION "2.17"
+#define PROGRAM_DATE "5/21/2018"
 /*****************************************************************************
  * userparse.h
  *
