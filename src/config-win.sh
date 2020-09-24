8cd2b935ce85e163187b5b11fa35861761f9eebc
arthur.taylor <arthur.taylor@noaa.gov>
2020-09-24 19:59:59 +0000

    MINGW - Better instructions for setting up a MinGW environment.
    git-svn-id: https://vlab.ncep.noaa.gov/svn/degrib/degrib@1409 98767674-7b1f-0410-897a-ff3155737c25

--- src/config-win.sh
+++ src/config-win.sh
@@ -1,5 +1,32 @@
 #!/bin/sh
 ./configure CFLAGS="-O3" TCL_PREFIX=../tcl853_include_win TCL_VERSION=85
+
+echo ""
+echo "NOTE: for MinGW we have the following hacks"
+echo ""
+echo "1) Error in /src/jpeg2000 :: "
+echo "   - aclocal: macro '_LT_COMPILER_PIC' required but not defined"
+echo "   - aclocal: macro '_LT_DECL_SED' required but not defined"
+echo "Solution:"
+echo "   $ cd /src/jpeg2000"
+echo "   $ vi Makefile"
+echo "   >> change 'run aclocal-1.6' to 'run aclocal-1.6a'"
+echo "   $ make"
+echo ""
+echo "2) Error in /src/jpeg2000 :: "
+echo "   - lib /OUT:.libs/libbase.lib  jas_cm.o ..."
+echo "   - ../../../libtool: line 4401: lib: command not found"
+echo "Solution:"
+echo "   $ cp /src/jpeg2000/libtool.mingw /src/jpeg2000/libtool"
+echo "   $ cd /src"
+echo "   $ make"
+echo ""
+echo "3) Error in /src/libxml :: "
+echo "   - ./libtool: line 1092: lib: command not found"
+echo "Solution:"
+echo "   $ cp /src/libxml/libtool.mingw /src/libxml/libtool"
+echo "   $ make"
+
 # ./configure CFLAGS="-O3" TCL_PREFIX=../tcl853_include_win TCL_VERSION=85 --with-halo
 
 #./configure CFLAGS="-O3" TCL_PREFIX=c:/Tcl859 TCL_VERSION=85
